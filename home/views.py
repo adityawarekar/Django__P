ac817a3530dc7851fbfe07ef857b7d0bb9bb55bc
Aditya <adityawarekar1984@gmail.com>
2025-08-04 19:23:40 +0530

    Initial commit

---
+++ home/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    peoples = [
+        {'name' : 'Aditya warekar', 'age' : 21},
+        {'name' : 'Deep zade', 'age' : 25},
+        {'name' : 'Ayush srode', 'age' : 17},
+        {'name' : 'Aniket gawande', 'age' : 20},
+
+    ]
+    vegetables = ['Pumpkin', 'Tomato', 'Potatoe']
+   
+    return render(request , "index.html", context = {'peoples' : peoples, 'vegetables' : vegetables})
+def about(request):
+    return render(request , "home/about.html")                    
+
+def contact(request):
+    return render(request , "home/contact.html") 
+
+def success_page(requests):
+    return HttpResponse("<h1>Hey this is is a Success page</h1>")
