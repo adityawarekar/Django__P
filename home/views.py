afa587a747be53431c535d9977a44a965d852ec6
Aditya <adityawarekar1984@gmail.com>
2025-10-29 19:51:12 +0530

    Deleted function run

--- home/views.py
+++ home/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from vege.seed import *
 
 def home(request):
+    seed_db(100)
+
     peoples = [
         {'name' : 'Aditya warekar', 'age' : 21},
         {'name' : 'Deep zade', 'age' : 25},
--- vege/models.py
+++ vege/models.py
@@ -1,18 +1,32 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.contrib.auth import get_user_model
+from django.conf import settings
 
-User = get_user_model()
+
+
+class ActiveManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted=False)
+
 
 class Receipe(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
     receipe_name = models.CharField(max_length=100)
     receipe_description = models.TextField()
     receipe_image = models.ImageField(upload_to="receipes/")
     receipe_view_count = models.IntegerField(default=0)
+    is_deleted = models.BooleanField(default=False)
+
+   
+    objects = ActiveManager()
+    
+    admin_objects = models.Manager()
 
     def __str__(self):
         return self.receipe_name
+
+    class Meta:
+        ordering = ["receipe_name"]
 
 
 class Department(models.Model):
@@ -22,34 +36,39 @@ class Department(models.Model):
         return self.department
 
     class Meta:
-        ordering = ['department']
+        ordering = ["department"]
 
 
-class StudentID(models.Model):   
+class StudentID(models.Model):
     student_id = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.student_id
 
+
 class Subject(models.Model):
     subject_name = models.CharField(max_length=100)
- 
+
+    def __str__(self):
+        return self.subject_name
 
 
-class Student(models.Model):   
+class Student(models.Model):
     department = models.ForeignKey(Department, related_name="depart", on_delete=models.CASCADE)
     student_id = models.OneToOneField(StudentID, related_name="studentid", on_delete=models.CASCADE)
     student_name = models.CharField(max_length=100)
     student_email = models.EmailField(unique=True)
     student_age = models.IntegerField(default=18)
     student_address = models.TextField()
+    is_deleted = models.BooleanField(default=False)
 
     def __str__(self):
         return self.student_name
 
     class Meta:
-        ordering = ['student_name']
+        ordering = ["student_name"]
         verbose_name = "Student"
+
 
 class SubjectMarks(models.Model):
     student = models.ForeignKey(Student, related_name="studentmarks", on_delete=models.CASCADE)
@@ -57,8 +76,7 @@ class SubjectMarks(models.Model):
     marks = models.IntegerField()
 
     def __str__(self):
-        return f'{self.student.student_name} - {self.subject.subject_name} ({self.marks})'
+        return f"{self.student.student_name} - {self.subject.subject_name} ({self.marks})"
 
     class Meta:
-        unique_together = ['student', 'subject']
-
+        unique_together = ["student", "subject"]
